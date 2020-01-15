2186fccb43b12f94139e604b09e2fac9c9e81cbe
Clint Moyer <contact@clintmoyer.com>
2020-01-14 20:44:13 -0700

    Split out modules

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 Cleanup your MacOS system
 
+## Building
+
+```
+$ swiftc *.swift -o shinyapple
+```
+
 ## Notes
 
 *  using `Foundation.Process()` rather than the Docker socket API because unable to find a stable socket library to use it
--- main.swift
+++ main.swift
@@ -1,71 +1,22 @@
 /*
 Copyright 2020 Clint Moyer
 
-This program is free software: you can redistribute it and/or modify
+This file is part of ShinyApple.
+
+ShinyApple is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
+ShinyApple is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
+along with ShinyApple.  If not, see <https://www.gnu.org/licenses/>.
 */
-import Foundation
-
-let dockerRuntime = "/usr/local/bin/docker"
-
-let fileManager = FileManager.default
-
-if !fileManager.fileExists(atPath: dockerRuntime) {
-	print("Missing Docker")
-	exit(1)
-}
-
-// docker ps -aq
-func getContainers() -> Array<String.SubSequence> {
-	let task = Process()
-	let pipe = Pipe()
-
-	task.standardOutput = pipe
-	task.executableURL = URL(fileURLWithPath: dockerRuntime)
-	task.arguments = ["ps", "-a", "-q"]
-
-	do {
-		try task.run()
-	} catch {
-	}
-
-	task.waitUntilExit()
-
-	let data = pipe.fileHandleForReading.readDataToEndOfFile()
-	let output = String(data: data, encoding: String.Encoding.utf8)!
-	let containers = output.split { $0.isNewline }
-
-	return containers
-}
-
-// docker rm <container>
-//
-// typically this clears data from /var/lib/docker/containers/
-// MacOS kernel cannot run Docker natively, instead using a VM
-// under $HOME/Library/Containers/com.docker.docker/Data
-//
-// ultimately this reduces the footprint of the Docker Machine
-func removeContainer(container: String.SubSequence) {
-	let task = Process()
-	task.executableURL = URL(fileURLWithPath: dockerRuntime)
-	task.arguments = ["rm", String(container)]
-	do {
-		try task.run()
-	} catch {
-	}
-	task.waitUntilExit()
-}
-
+checkDocker()
 let containers = getContainers()
 for container in containers {
 	removeContainer(container: container)
